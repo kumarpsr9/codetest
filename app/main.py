3d55ded934c54eb48066984fee23a1764edd6f3a
kumarpsr <kumarpsr9@gmail.com>
2023-09-05 12:54:18 +0530

    Docker added

--- app/main.py
+++ app/main.py
@@ -21,15 +21,18 @@ def generate_random_string(length):
 
 # Define allowed programming languages and their respective Docker images
 languages = {
-    "python": "python:3.9-slim",
-    "c": "clang",
-    "cpp": "gcc:11",
+    "python": "py",
+    "c": "c",
+    "cpp": "cpp",
+    "java":"java"
 }
 
 temp_dir="/app/temp/"
 hostname=socket.gethostname()
+
 # Function to compile code using Docker container
 def compile_code(language, code):
+    ramdomstring=generate_random_string(10)
     code=base64.b64decode(code).decode('utf-8')
     #print(code)
     if language not in languages:
@@ -37,8 +40,8 @@ def compile_code(language, code):
 
     # Create a temporary directory to store code
     #with tempfile.TemporaryDirectory() as temp_dir:
-    code_file = os.path.join(temp_dir, f"code_{hostname}_{generate_random_string(10)}.{language}")
-    output_file = os.path.join(temp_dir, f"output_{hostname}_{generate_random_string(10)}")
+    code_file = os.path.join(temp_dir, f"code_{hostname}_{ramdomstring}.{language}")
+    output_file = os.path.join(temp_dir, f"output_{hostname}_{ramdomstring}")
     print(code_file,output_file)
 
     # Write code to a temporary file
