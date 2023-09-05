1ae6b2d09b69663cfe2dab840bdac21e137ef544
kumarpsr <kumarpsr9@gmail.com>
2023-09-05 12:31:37 +0530

    Docker added

--- app/main.py
+++ app/main.py
@@ -1,29 +1,45 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import subprocess
 import tempfile
 import os
 import base64
-
+import socket
 app = FastAPI()
+import random
+import string 
+
+
+def generate_random_string(length):
+    # Define the characters you want to include in the random string
+    characters = string.ascii_letters + string.digits  # You can customize this to include other characters if needed
+
+    # Generate a random string of the specified length
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    
+    return random_string
 
 # Define allowed programming languages and their respective Docker images
 languages = {
     "python": "python:3.9-slim",
-    "c": "gcc:11",
+    "c": "clang",
     "cpp": "gcc:11",
 }
-temp_dir="temp"
+
+temp_dir="/app/temp/"
+hostname=socket.gethostname()
 # Function to compile code using Docker container
 def compile_code(language, code):
     code=base64.b64decode(code).decode('utf-8')
-    print(code)
+    #print(code)
     if language not in languages:
         raise HTTPException(status_code=400, detail="Unsupported language")
 
     # Create a temporary directory to store code
     #with tempfile.TemporaryDirectory() as temp_dir:
-    code_file = os.path.join(temp_dir, f"code.{language}")
-    print(code_file)
+    code_file = os.path.join(temp_dir, f"code_{hostname}_{generate_random_string(10)}.{language}")
+    output_file = os.path.join(temp_dir, f"output_{hostname}_{generate_random_string(10)}")
+    print(code_file,output_file)
 
     # Write code to a temporary file
     with open(code_file, "w") as file:
@@ -34,25 +50,30 @@ def compile_code(language, code):
     if language == "python":
         command = ["python", code_file]
     elif language == "c":
-        command = ["gcc", code_file, "-o", os.path.join(temp_dir, "output")]
+        command = ["gcc", code_file, "-o",output_file]
     elif language == "cpp":
-        command = ["g++", code_file, "-o", os.path.join(temp_dir, "output")]
+        command = ["g++", code_file, "-o", output_file]
 
     try:
         # Run the Docker container to compile the code
-        output=subprocess.run(["docker", "run", "--rm", "-v", f"./{temp_dir}:/{temp_dir}", image] + command, check=True)
-        print(temp_dir)
+        #print(command)
+        output=subprocess.run(command, check=True)
+        #print(temp_dir)
         # Read and return the compiled output
         if language == "c" or language == "cpp":
-             output = subprocess.run(["sh", os.path.join(temp_dir, "output")], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-        return output
+            output=subprocess.run([output_file],stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True).stdout
+        return {"output": output, "hostname":socket.gethostname()}
     except subprocess.CalledProcessError as e:
         raise HTTPException(status_code=400, detail="Compilation failed")
 
-@app.post("/compile/{language}")
-async def compile_language(language: str, code: str):
+class Payload(BaseModel):
+    language: str
+    code: str
+
+@app.post("/compile")
+async def compile_language(payload: Payload):
     try:
-        output = compile_code(language, code)
+        output = compile_code(payload.language, payload.code)
         return {"output": output}
     except HTTPException as e:
         raise e
